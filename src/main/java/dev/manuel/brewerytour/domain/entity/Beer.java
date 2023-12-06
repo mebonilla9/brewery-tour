1ab30e91dd442239951a211acc77046af8e645ea
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2023-12-06 12:33:50 -0500

    Added the entity for the Beers

--- src/main/java/dev/manuel/brewerytour/domain/entity/Beer.java
+++ src/main/java/dev/manuel/brewerytour/domain/entity/Beer.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
-import java.util.List;
 import java.util.Objects;
 
 @Getter
@@ -14,28 +13,26 @@ import java.util.Objects;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "brewery")
-public class Brewery {
+@Table(name = "beer")
+public class Beer {
 
   @Id
   @SequenceGenerator(
-    name = "brewery_id_sequence",
-    sequenceName = "brewery_id_sequence"
+    name = "beer_id_sequence",
+    sequenceName = "beer_id_sequence"
   )
   @GeneratedValue(
     strategy = GenerationType.SEQUENCE,
-    generator = "brewery_id_sequence"
+    generator = "beer_id_sequence"
   )
   private Integer id;
   private String name;
-  private String location;
-  private String image;
-  private String address;
-  private String phone;
+  private Double price;
+  private Integer availableQuantity;
 
-  @OneToMany
-  @ToString.Exclude
-  private List<Beer> beerList;
+  @ManyToOne
+  @JoinColumn(name = "brewery_id")
+  private Brewery brewery;
 
   @Override
   public final boolean equals(Object o) {
@@ -44,8 +41,8 @@ public class Brewery {
     Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
     Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
     if (thisEffectiveClass != oEffectiveClass) return false;
-    Brewery brewery = (Brewery) o;
-    return getId() != null && Objects.equals(getId(), brewery.getId());
+    Beer beer = (Beer) o;
+    return getId() != null && Objects.equals(getId(), beer.getId());
   }
 
   @Override
